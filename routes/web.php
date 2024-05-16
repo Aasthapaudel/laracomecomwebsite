f5c98f559fca0b0b54821826dfa3dc87780732c4
Aastha <pdlpangali111@gmail.com>
2024-05-16 11:10:22 +0545

    admin panel setup with simple route and controller

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\ShoppingController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
+use App\Http\Controllers\adminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,9 +17,9 @@ use App\Models\Product;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+|cost
 */
-
+//costumer site
 Route::get('/home', function () {
     $products=Product::all();
     return view('theme.home',compact('products'));
@@ -51,6 +52,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//admin site
+Route::get('/admin',[adminController::class,'index']);
+
 
 
 require __DIR__.'/auth.php';
