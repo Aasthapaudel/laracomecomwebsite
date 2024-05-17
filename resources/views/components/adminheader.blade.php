1219491180a54482d43712a75fe80e1eb80b8457
Aastha <pdlpangali111@gmail.com>
2024-05-17 12:36:50 +0545

    admin panel update

---
+++ resources/views/components/adminheader.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Menswear admin</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="admin dash/vendors/feather/feather.css">
+  <link rel="stylesheet" href="admin dash/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="admin dash/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="admin dash/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+  <link rel="stylesheet" href="admin dash/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" type="text/css" href="admin dash/js/select.dataTables.min.css">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="admin dash/css/vertical-layout-light/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="admin dash/images/favicon.png" />
+  <link href="{{asset('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
